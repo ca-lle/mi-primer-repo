fce069797b01338b4108d3fed8a2094643775678
ca-lle <david.calle@plazacastilla.salesianas.org>
2025-04-24 12:28:47 +0200

    Añadir proyecto
    Añadir proyecto java al repositorio

---
+++ E01.java
@@ -0,0 +1,21 @@
+package fechas;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class E01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner sc=new Scanner(System.in);
+	LocalDate hoy = LocalDate.now();
+	DayOfWeek dia = hoy.getDayOfWeek();
+	System.out.println(hoy +" "+ dia);
+	
+	
+	
+	}
+	
+}
+//1. Muestra el día, mes y año actual, incluido el día de la semana.
