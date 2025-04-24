878885d46b3be7d627735ef1f9ceaa71ef555cec
daw1 <daw202604@estudiantes.salesianasnsp.es>
2025-04-24 13:04:36 +0200

    añadir otro projecto

---
+++ E02.java
@@ -0,0 +1,11 @@
+package fechas;
+
+public class E02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+//2. Lee dos fechas de teclado (día, mes y año) y ordénalas de menor a mayor.
